393fd2dccefb79f8e7ef1a7a6643df9fb681c290
Serg <64j@mail.ru>
2019-04-21 23:18:40 +0300

    add spanish lang

---
+++ assets/plugins/templatesedit/lang/spanish.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'role' => 'Rol',
+    'fields' => 'Campos base',
+    'tmplvars' => 'Parámetros (TV)',
+    'categories' => 'Categorías',
+    // action
+    'action.empty' => 'Vacío',
+    'action.del' => 'Borrar',
+    'action.get' => 'Obtener la configuración predeterminada',
+    'action.set' => 'Establecer',
+    'action.set_default' => 'Predeterminado para todos',
+    'action.del_default' => 'Eliminar configuración predeterminada',
+    // confirm
+    'confirm.del' => 'Atención! \n¿Estás seguro de que quieres eliminar la configuración?',
+    'confirm.del_default' => 'Atención! \nEsto eliminará la plantilla global.',
+    'confirm.set_default' => 'Atención! \nEsto sobrescribirá la plantilla global de forma predeterminada y se usará para documentos con una plantilla en blanco.',
+    // info
+    'info.used_default_config' => 'La configuración se usa por defecto',
+    'info.used_config_role_admin' => 'Usar la configuración del rol de administrador',
+    'info.default_template' => 'Esta configuración se utiliza como global.',
+    'info.there_is_config' => 'Hay una configuración guardada para esta plantilla.'
+];
