3918e50b03804c83b8c7d55f1516bbe33ba0b0c3
Serg <64j@mail.ru>
2020-10-09 16:08:40 +0300

    Create nederlands-utf8

---
+++ assets/plugins/templatesedit/lang/nederlands-utf8.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'role' => 'Role',
+    'fields' => 'Standaard velden',
+    'tmplvars' => 'Parameters (TV)',
+    'categories' => 'Categorieën',
+    // action
+    'action.empty' => 'Legen',
+    'action.del' => 'Verwijderen',
+    'action.get' => 'Standaard configuratie gebruiken',
+    'action.set' => 'Instellen',
+    'action.set_default' => 'Standaard voor alle',
+    'action.del_default' => 'Verwijder standaard configuratie',
+    // confirm
+    'confirm.del' => 'Let op! \nWeet u zeker dat u de configuratie wilt verwijderen?',
+    'confirm.del_default' => 'Let op! \nDit zal de standaard template verwijderen.',
+    'confirm.set_default' => 'Let op! \nDit zal standaard de algemene template overschrijven en worden gebruikt voor documenten met een lege template.',
+    // info
+    'info.used_default_config' => 'Configuratie wordt standaard gebruikt',
+    'info.used_config_role_admin' => 'Gebruik beheerdersrol configuratie',
+    'info.default_template' => 'Deze configuratie wordt gebruikt als globaal.',
+    'info.there_is_config' => 'Er is een opgeslagen configuratie voor deze sjabloon.',
+    // import / export
+    'import_export' => 'Importeren / Exporteren',
+    'import.btn' => 'Importeren',
+    'export.btn' => 'Exporteren',
+];
